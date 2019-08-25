49fde47c8ce05f099b56a934426d1d22d83ed1c7
Habib Rahad <habib.reza.karim@gmail.com>
2019-08-26 01:31:49 +0600

    4.0.0

--- app/src/main/java/com/darkwhite/myweatherapp/MainActivity.java
+++ app/src/main/java/com/darkwhite/myweatherapp/MainActivity.java
@@ -8,7 +8,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.text.method.LinkMovementMethod;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
